9d3f2f0f8c26069efb9a086119396f52c1a1955e
Doug Wollison <doug@wollison.net>
2017-08-25 10:17:31 -0400

    Renaming and restructuring part 2
    Getting real tired of this not working in 1 commit

--- bin/shared.py
+++ bin/shared.py
@@ -8,9 +8,9 @@ project = 'default'
 if len( sys.argv ) > 1 :
 	project = sys.argv[1]
 
-dir = os.path.dirname( os.path.realpath( __file__ ) )
+dir = os.path.dirname( os.path.dirname( os.path.realpath( __file__ ) ) ) + '/conf'
 
-conf_main = dir + '/tym.conf'
+conf_main = dir + '/default.conf'
 conf_project = dir + '/' + project + '.conf';
 
 config.read( conf_main )
