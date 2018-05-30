71e774e700ebd111cf1d5331068f411fc956e79e
Doug Wollison <doug@wollison.net>
2018-05-30 14:31:31 -0400

    Replace os.path.join with custom pathit tool
    Automatically strips shashes on either side of part before combining; no double slashes, no rendering previous parts moot just because a piece starts with /

--- bin/shared.py
+++ bin/shared.py
@@ -1,8 +1,6 @@
 import sys
-from configparser import ConfigParser
 import os
-
-config = ConfigParser()
+import configparser
 
 project = 'default'
 if len( sys.argv ) > 1 :
@@ -12,19 +10,29 @@ option = None
 if len( sys.argv ) > 2 :
 	option = sys.argv[2]
 
-backit_dir = os.path.realpath( os.path.join( __file__, '../../' ) )
-conf_dir = os.path.join( backit_dir, 'conf' )
-
-# Read the default config if found
-conf_main = os.path.join( conf_dir, 'default.conf' )
-if os.path.isfile( conf_main ) :
-	config.read( conf_main )
-
-# Read the project config if found
-conf_project = os.path.join( conf_dir, project + '.conf' )
-if os.path.isfile( conf_project ) :
-	config.read( conf_project )
+config = configparser.ConfigParser()
 
 # Alias to ConfigParser.get()
 def get( section, name, default=None ) :
 	return config.get( section, name, fallback=default )
+
+# Build a directory path
+def pathit( start, *parts ) :
+	path = [ start ]
+	for part in parts :
+		path.append( part.strip( '/' ) )
+
+	return '/'.join( path )
+
+backit_dir = os.path.realpath( pathit( __file__, '../../' ) )
+conf_dir = pathit( backit_dir, 'conf' )
+
+# Read the default config if found
+conf_main = pathit( conf_dir, 'default.conf' )
+if os.path.isfile( conf_main ) :
+	config.read( conf_main )
+
+# Read the project config if found
+conf_project = pathit( conf_dir, project + '.conf' )
+if os.path.isfile( conf_project ) :
+	config.read( conf_project )
