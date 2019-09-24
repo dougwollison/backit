c3530e93fbe6f8f3f7757b6cf0ae714a40042797
Doug Wollison <doug@wollison.net>
2019-09-23 20:14:56 -0400

    replace flag system with backup json records
    one file for the whole backup folder, storing current status and file statuses

---
+++ bin/job.py
@@ -0,0 +1,39 @@
+import os
+import json
+
+class JobFile :
+	def __init__( self, folder, initStatus = None ) :
+		"""Load the status file, or create if defaults are provided"""
+
+		self.file = folder + '.json'
+		self.data = {
+			'status': initStatus,
+			'files': {}
+		}
+
+		if os.path.isfile( self.file ) :
+			input = open( self.file )
+			self.data = json.load( input )
+			input.close()
+
+		elif initStatus != None :
+			self.save()
+
+	def save( self ) :
+		with open( self.file, 'w' ) as output :
+			json.dump( self.data, output )
+			output.close()
+
+	def isStatus( self, status ) :
+		return self.data['status'] == status
+
+	def setStatus( self, status ) :
+		self.data['status'] == status
+		self.save()
+
+	def isFile( filename, status ) :
+		return self.data['files'][ filename ] == status
+
+	def logFile( filename, status ) :
+		self.data['files'][ filename ] == status
+		self.save()
